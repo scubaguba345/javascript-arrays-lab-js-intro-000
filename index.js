844be0cee83e14a72c8514a0e17fd10b7e4233b4
brandon gubitosa <brandon.gubitosa@gmail.com>
2020-02-09 00:57:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,3 @@ var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 function destructivelyAppendKitten(name){
   return kittens.push(name)
 }
-function destructivelyAppendKitten(name){
-  return kittens.unshift(name)
-}
